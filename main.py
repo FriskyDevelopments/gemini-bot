630f1b56e405a30eb5814e357d637c787b3ae33c
Frisky Pup <friskypup@Friskys-MacBook-Pro.local>
2026-04-03 14:19:56 -0600

    Terminal Hack: Added Flask stay-awake server

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+import os
 import sys
 import google.generativeai as genai
 from telegram import Update
